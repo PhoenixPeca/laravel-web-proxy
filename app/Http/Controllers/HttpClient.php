27ae707907c3b73637c4a9057d6cf9276ee4b7ba
Phoenix Eve Aspacio <aspaciop@outlook.com>
2020-05-20 15:00:24 +0800

    [wip] controller httpclient

--- app/Http/Controllers/HttpClient.php
+++ app/Http/Controllers/HttpClient.php
@@ -10,10 +10,15 @@ use GuzzleHttp\Cookie\CookieJar;
 class HttpClient extends Controller
 {
 
-    private $client, $request, $remote_url, $remote_cookies, $local_cookie_jar;
+    private $client, $local_base_url, $local_full_url, $request, $remote_url, $remote_cookies, $local_cookies, $cookie_jar;
 
     public function intialize($scheme, $site, $path = null) {
-        $this->remote_url = $scheme.'://'.$site.'/'.$path.'?'.http_build_query($_GET);
+        $_getQ = (!empty($_GET) ? '?'.http_build_query($_GET) : '');
+
+        $this->remote_url = $scheme.'://'.$site.'/'.$path.$_getQ;
+
+        $this->local_base_url = '/'.$scheme.'/'.$site;
+        $this->local_full_url = $this->local_base_url.'/'.$path.$_getQ;
 
         $this->client = new \GuzzleHttp\Client([
             'allow_redirects' => [
@@ -23,15 +28,18 @@ class HttpClient extends Controller
             'cookies' => true
         ]);
 
-        $this->request = $this->client->request('GET', $this->remote_url);
+        $this->get_local_cookies_arr();
+        $this->set_outbound_cookie_payload();
+
+        $this->request = $this->client->request('GET', $this->remote_url, [
+            'cookies' => $this->cookie_jar
+        ]);
 
         $this->remote_cookies = $this->client->getConfig('cookies')->toArray();
 
         if ($this->redirect_to() !== false) {
             return redirect($this->redirect_to(), 302);
         }
-
-        $this->local_cookies();
 
         return $this->http_response();
     }
@@ -41,7 +49,7 @@ class HttpClient extends Controller
             \GuzzleHttp\RedirectMiddleware::HISTORY_HEADER
         );
 
-        if (count($redirect_hitory) > 1) {
+        if (!empty($redirect_hitory)) {
             $remote_url_final = end($redirect_hitory);
 
             $url = Url::fromString($remote_url_final);
@@ -50,7 +58,8 @@ class HttpClient extends Controller
                 $url->getHost(),
                 ltrim($url->getPath(), '/')
             ];
-            return implode('/', $url_parts).'?'.$url->getQuery();
+            $_getQ = (!empty($url->getQuery()) ? '?'.http_build_query($url->getQuery()) : '');
+            return implode('/', $url_parts).$_getQ;
         } else {
             return false;
         }
@@ -60,25 +69,34 @@ class HttpClient extends Controller
         $result = response($this->request->getBody(), $this->request->getStatusCode());
         $result->header('Content-Type', $this->request->getHeaderLine('Content-Type'));
         foreach ($this->remote_cookies as $cookie) {
-            $result->cookie(base64_encode($cookie['Domain']).'|'.base64_encode($cookie['Name']), $cookie['Value'], $cookie['Max-Age']);
+            $result->cookie(base64_encode($cookie['Domain']).'|'.base64_encode($cookie['Name']), $cookie['Value'], $cookie['Max-Age'], $this->local_base_url.$cookie['Path']);
         }
         return $result;
     }
 
-    private function local_cookies() {
+    private function get_local_cookies_arr() {
         $cookies = Cookie::get();
         $local_cookies = [];
         foreach ($cookies as $label => $value) {
             $label_set = explode('|', $label, 2);
             if (count($label_set) === 2) {
                 $local_cookies[] = [
-                    'Domain' => base64_decode($label_set[0]),
+                    'Domain' => base64_decode($label_set[0]), // not being used but may help in future usecase
                     'Name' => base64_decode($label_set[1]),
                     'Value' => $value
                 ];
             }
         }
-        // $this->local_cookie_jar = CookieJar::fromArray($local_cookies);
-        dd($this->remote_cookies);
+        $this->local_cookies = $local_cookies;
     }
+
+    private function set_outbound_cookie_payload() {
+        $url = Url::fromString($this->remote_url);
+        $cookies = [];
+        foreach ($this->local_cookies as $cookie) {
+            $cookies[$cookie['Name']] = $cookie['Name'];
+        }
+        $this->cookie_jar = CookieJar::fromArray($cookies, $url->getHost());
+    }
+
 }
